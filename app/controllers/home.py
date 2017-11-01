2ee22407f1d49a60f31e20a1a212eb818644b46a
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2017-11-01 20:40:52 +0300

    Initial Implementation using blueprints

---
+++ app/controllers/home.py
@@ -0,0 +1,12 @@
+from flask import render_template, Blueprint
+
+home_blueprint = Blueprint(
+    'home',
+    __name__,
+    template_folder="../templates/home"
+)
+
+ 
+@home_blueprint.route('/')
+def index():
+    return render_template("index.html")
