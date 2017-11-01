d3f9308e81b96fc8295d523a98cc0466faee4f47
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2017-11-01 22:12:04 +0300

    Add login and regiter routes

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -6,7 +6,16 @@ home_blueprint = Blueprint(
     template_folder="../templates/home"
 )
 
- 
+
 @home_blueprint.route('/')
 def index():
     return render_template("index.html")
+
+@home_blueprint.route('/register')
+def register():
+    return render_template('index.html')
+
+@home_blueprint.route('/login')
+def login():
+    return render_template('index.html')
+
