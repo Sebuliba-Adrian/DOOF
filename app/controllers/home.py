39766843faf28b207eab43598de369c0d2944174
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2017-11-02 03:48:59 +0300

    Imolement sign in route and sign up

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -1,4 +1,7 @@
-from flask import render_template, Blueprint
+""" This module contains all the view functions for the recipes app"""
+from flask import render_template, Blueprint, session, redirect, url_for, request, flash
+from app.utilities import register, login
+from app import USERS
 
 home_blueprint = Blueprint(
     'home',
@@ -9,13 +12,27 @@ home_blueprint = Blueprint(
 
 @home_blueprint.route('/')
 def index():
-    return render_template("index.html")
+    """Handle the index route"""
+    if session.get('username'):
+        return redirect(url_for('read_categories'))
+    else:
+        return redirect(url_for('sign_in'))
 
-@home_blueprint.route('/register')
-def register():
+
+@home_blueprint.route('/register', methods=["GET", "POST"])
+def sign_up():
+    """ Handles the registeration route """
     return render_template('index.html')
 
-@home_blueprint.route('/login')
-def login():
-    return render_template('index.html')
 
+@home_blueprint.route('/login', methods=['GET', 'POST'])
+def sign_in():
+    """ Handles the sign_in route """
+    if request.method == 'POST':
+        result = login(request.form['username'], request.form['password'])
+        if result == "Login successful":
+            session['username'] = request.form['username']
+            return redirect(url_for('read_buckets'))
+        flash(result, 'warning')
+    return render_template('login.html')
+    
