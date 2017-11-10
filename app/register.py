9e89ee4cc662364a384170e9349f8ce678c55484
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2017-11-10 07:26:51 +0300

    Update folder structuring and refactor code

--- app/register.py
+++
@@ -1,6 +0,0 @@
-from app import app
-from flask import Flask, request, session, g, redirect, url_for, abort,render_template, flash, _app_ctx_stack
-
-@app.route('/register')
-def register():
-    return render_template('register.html')
